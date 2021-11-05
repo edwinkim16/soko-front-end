a54940a163f8823e029e2c1f180c37b4673dc74c
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-11-05 08:23:12 +0300

    adding about us information and adding the component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   {path:'home',component:HomeComponent},
+  {path:'about',component:AboutComponent}
 ];
 
 @NgModule({
